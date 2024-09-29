909db07904f5675bdf3f7ddfd53e3ad6de238add
souoGael <2494554@students.wits.ac.za>
2024-09-29 22:53:56 +0200

    Blessing is difficult

--- formation/Formation.py
+++ formation/Formation.py
@@ -3,17 +3,17 @@ import numpy as np
 def GenerateBasicFormation():
 
     formation = [
-        np.array([-13, 0]),    # Goalkeeper
-        np.array([-10, 6]),  # Left Defender
-        np.array([-10, 2]),   # Center Back Left
-        np.array([-10, -2]),    # Center Back Right
-        np.array([-10, -6]),   # Right Defender
-        np.array([1, 5]),    # Left Midfielder
-        np.array([1, 0]),    # Center Midfielder Left
-        np.array([1, -5]),     # Center Midfielder Right
-        np.array([8, 0]),     # Right Midfielder
-        np.array([9, 1]),    # Forward Left
-        np.array([12, 0])      # Forward Right
+        np.array([-13, 0]),    # Goalkeeper                p1
+        np.array([-5, 6]),    # Left Defender              p2
+        np.array([-8, 2]),   # Center Back Left            p3
+        np.array([-8, -2]),    # Center Back Right         p4
+        np.array([-5, -6]),   # Right Defender             p5
+        np.array([3, 5]),      # Left Midfielder           p6 
+        np.array([1, 0]),      # Center Midfielder Left    p7
+        np.array([3, -5]),      # Center Midfielder Right  p8
+        np.array([7, 4]),      # Right Midfielder          p9    
+        np.array([9, 0]),      # Forward Left              p10
+        np.array([7, -4])      # Forward Right             p11
     ]
 
     return formation
